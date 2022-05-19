b13b1b5d1efba9cb9e4fcebde667bdefd54113fc
Mattis-Schulte <MattisSchulte@hotmail.de>
2022-05-19 22:28:16 +0200

    Ajax

--- intern_szut/inventory_management/views.py
+++ intern_szut/inventory_management/views.py
@@ -6,19 +6,41 @@ def index(request):
     return redirect('overview')
 
 def overview(request):
-    return render(request, 'overview.html')
+    # If request is ajax
+    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+        return render(request, 'overview.html')
+    else:
+        return render(request, 'index.html', {'current_page': 'overview', 'current_page_file': 'overview.html'})
 
 def rooms(request):
-    return render(request, 'rooms.html')
+    # If request is ajax
+    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+        return render(request, 'rooms.html')
+    else:
+        return render(request, 'index.html', {'current_page': 'rooms', 'current_page_file': 'rooms.html'})
 
 def devices(request):
-    return render(request, 'devices.html')
+    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+        return render(request, 'devices.html')
+    else:
+        return render(request, 'index.html', {'current_page': 'devices', 'current_page_file': 'devices.html'})
 
 def ticket_management(request):
-    return render(request, 'ticket_management.html')
+    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+        return render(request, 'ticket_management.html')
+    else:
+        return render(request, 'index.html', {'current_page': 'ticket_management', 'current_page_file': 'ticket_management.html'})
 
 def page_not_found_view(request, exception):
-    return redirect('overview')
+    # If request is ajax
+    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+        return HttpResponse(status=404)
+    else:
+        return redirect('overview')
 
 def page_error(request):
-    return redirect('overview')
+    # If request is ajax
+    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+        return HttpResponse(status=500)
+    else:
+        return redirect('overview')
