9d0ece2121ba174e4c4f29d12fa018a5ccf5999f
Sajan Sivapatham <sajan1@schule.bremen.de>
2022-05-23 13:23:16 +0200

    database

--- intern_szut/inventory_management/admin.py
+++ intern_szut/inventory_management/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Raeume, Geraete_Klasse
 
 # Register your models here.
+admin.site.register(Raeume)
+admin.site.register(Geraete_Klasse)
--- intern_szut/inventory_management/models.py
+++ intern_szut/inventory_management/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Raeume(models.Model):
+    name = models.CharField(max_length=10, default=None)
+    bauabschnitt = models.CharField(max_length=10, default=None)
+    etage = models.IntegerField(default=None)
+
+
+class Geraete_Klasse(models.Model):
+    geraete_klasse_name = models.CharField(default=None)
+    icon = models.ImageField(upload_to='static/images', default=None)
+
