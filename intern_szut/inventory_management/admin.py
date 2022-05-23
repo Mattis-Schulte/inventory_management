b71f0064190c8868cb5f1122f8e0dda9cab1c427
Sajan Sivapatham <sajan1@schule.bremen.de>
2022-05-23 23:25:27 +0200

    database

--- intern_szut/inventory_management/admin.py
+++ intern_szut/inventory_management/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Raeume, Geraete_Klasse
+from .models import Raeume, Geraete_Klasse, Geraete_Typen, Tickets
 
 # Register your models here.
 admin.site.register(Raeume)
 admin.site.register(Geraete_Klasse)
+admin.site.register(Geraete_Typen)
+admin.site.register(Tickets)
+
--- intern_szut/inventory_management/models.py
+++ intern_szut/inventory_management/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
+# id's nicht enthalten
 
 class Raeume(models.Model):
     name = models.CharField(max_length=10, default=None)
@@ -10,6 +10,35 @@ class Raeume(models.Model):
 
 
 class Geraete_Klasse(models.Model):
-    geraete_klasse_name = models.CharField(default=None)
+    geraete_klasse_name = models.CharField(max_length=10, default=None)
     icon = models.ImageField(upload_to='static/images', default=None)
+
+class Geraete_Typen(models.Model):
+    geraete_typen_name = models.CharField(max_length=10, default=None)
+    geraete_klasse = models.ForeignKey(Typ_Name, on_delete=models.CASCADE)
+
+#class Typ_Name(models.Model):
+#    name = models.CharField(max_length=10, default=None)
+#    status = models.CharField(max_length=10, default=None)
+#    kosten = models.IntegerField(default=None)
+#    hersteller = models.CharField(max_length=10, default=None)
+#    anschaffungsdatum = models.DateField(default=None)
+#    seriennummer = models.CharField(max_length=50, default=None)
+#    garantiedauer = models.IntegerField(default=None)
+#    garantie_informations = models.CharField(max_length=50, default=None)
+#    geraete_klasse = models.ForeignKey(Geraete_Klasse, on_delete=models.CASCADE)
+#    geraete_typen = models.ForeignKey(Geraete_Typen, on_delete=models.CASCADE)
+
+class Tickets(models.Model):
+    ticket_name = models.CharField(Raeume, on_delete=models.CASCADE)
+    ticket_header = models.CharField(max_length=10, default=None)
+    ticket_message = models.CharField(max_length=100, default=None)
+    ticket_typ_name = models.ForeignKey(Typ_Name, on_delete=models.CASCADE)
+
+
+
+
+
+
+
 
