4ebf577cad85845324195e4649a21dfb519fa6c0
Mattis-Schulte <MattisSchulte@hotmail.de>
2022-05-25 20:39:08 +0200

    css

--- intern_szut/static/js/main.js
+++ intern_szut/static/js/main.js
@@ -1,7 +1,7 @@
 var current_highlight;
 
 $(document).ready(function() {
-  $("#login-form").on('submit', function(e){
+  $('#login-form').on('submit', function(e){
      e.preventDefault();
         var form = $(this);
         $.ajax({
@@ -19,6 +19,28 @@ $(document).ready(function() {
             }
         });
   });
+
+  $('.menu-button-wrapper').click(function() {
+      $('.account-card').hide();
+      $('.top-bar-login').attr('aria-pressed', 'false');
+      $('.top-bar-login i').removeClass('bi-chevron-up');
+
+      $('.menu-button-wrapper').attr('aria-pressed', function(i, attr) {
+            return attr == 'true' ? 'false' : 'true';
+      });
+      $('.nav-side-bar').toggleClass('nav-side-bar-open');
+  });
+
+  $('.top-bar-login').click(function() {
+      $('.nav-side-bar').removeClass('nav-side-bar-open');
+      $('.menu-button-wrapper').attr('aria-pressed', 'false');
+
+      $('.account-card').toggle();
+      $('.top-bar-login i').toggleClass('bi-chevron-up');
+      $('.top-bar-login').attr('aria-pressed', function(i, attr) {
+            return attr == 'true' ? 'false' : 'true';
+      });
+  });
 });
 
 loadAjaxContent = function(page) {
@@ -29,12 +51,14 @@ loadAjaxContent = function(page) {
             $('#custom-css').remove();
             $('#page-content').html(data);
             $('#load-content-error').hide();
+            $('.nav-side-bar').removeClass('nav-side-bar-open');
             window.history.pushState(null, null, '/' + page + '/');
             setHighlight(page);
             },
         error: function() {
             $('#custom-css').remove();
             loadContentError();
+            $('.nav-side-bar').removeClass('nav-side-bar-open');
             window.history.pushState(null, null, '/' + page + '/');
             setHighlight(page);
             },
@@ -45,7 +69,7 @@ loadAjaxContent = function(page) {
 }
 
 loadContentError = function() {
-    document.title = "Fehler – SZUT Inventur Management";
+    document.title = 'Fehler – SZUT Inventur Management';
     $('#load-content-error').show();
     $('#page-content').html('');
 }
